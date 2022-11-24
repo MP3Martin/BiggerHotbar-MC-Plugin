7a83fd9057b24355755e6bfa6b08cd49f2d58e50
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-11-24 11:35:04 +0100

    Better comments in BiggerHotbar class

--- src/main/java/xyz/mp3martin/biggerhotbar/biggerhotbar/BiggerHotbar.kt
+++ src/main/java/xyz/mp3martin/biggerhotbar/biggerhotbar/BiggerHotbar.kt
@@ -5,12 +5,17 @@ import xyz.mp3martin.biggerhotbar.biggerhotbar.commands.BiggerhotbarCommand
 
 class BiggerHotbar : JavaPlugin() {
   override fun onEnable() {
-    // Plugin startup logic
-    logger.info("Hello World!!")
+    // Plugin setup
+    logger.info("BiggerHotbar is working!")
     getCommand("biggerhotbar")!!.executor = BiggerhotbarCommand(this)
     this.server.pluginManager.registerEvents(EventListener(this), this)
 
-    val configItemsMap = mapOf("bh_enabled" to false, "maxMovesAtOnce" to 4, "mode" to "center")
+    // Config setup
+    val configItemsMap = mapOf(
+      "bh_enabled" to false,
+      "maxMovesAtOnce" to 4,
+      "mode" to "center")
+
     configItemsMap.forEach { entry ->
       config.addDefault(entry.key, entry.value)
     }
