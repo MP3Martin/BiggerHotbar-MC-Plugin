05c0dc27347e903f78f3106c82f016b9f4fda8ef
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-12-01 22:28:34 +0100

    Remove repeating using for loop

--- src/main/java/xyz/mp3martin/biggerhotbar/biggerhotbar/utils/MoveItemsHotbarInvSmall.kt
+++ src/main/java/xyz/mp3martin/biggerhotbar/biggerhotbar/utils/MoveItemsHotbarInvSmall.kt
@@ -28,43 +28,23 @@ fun moveItemsHotbarInvSmall(plugin: BiggerHotbar, player: Player, direction: Int
   }
   if (direction == 0) {
     moveItem(0, 27)
-    moveItem(1, 0)
-    moveItem(2, 1)
-    moveItem(3, 2)
-    moveItem(4, 3)
-    moveItem(5, 4)
-    moveItem(6, 5)
-    moveItem(7, 6)
-    moveItem(8, 7)
+    for (i in 1..8) {
+        moveItem(i, i-1)
+    }
 
-    moveItem(27, 28)
-    moveItem(28, 29)
-    moveItem(29, 30)
-    moveItem(30, 31)
-    moveItem(31, 32)
-    moveItem(32, 33)
-    moveItem(33, 34)
-    moveItem(34, 35)
+    for (i in 27..34) {
+        moveItem(i, i+1)
+    }
     moveItem(35, 8)
   } else {
     moveItemReverse(0, 27)
-    moveItemReverse(1, 0)
-    moveItemReverse(2, 1)
-    moveItemReverse(3, 2)
-    moveItemReverse(4, 3)
-    moveItemReverse(5, 4)
-    moveItemReverse(6, 5)
-    moveItemReverse(7, 6)
-    moveItemReverse(8, 7)
+    for (i in 1..8) {
+        moveItemReverse(i, i-1)
+    }
 
-    moveItemReverse(27, 28)
-    moveItemReverse(28, 29)
-    moveItemReverse(29, 30)
-    moveItemReverse(30, 31)
-    moveItemReverse(31, 32)
-    moveItemReverse(32, 33)
-    moveItemReverse(33, 34)
-    moveItemReverse(34, 35)
+    for (i in 27..34) {
+        moveItemReverse(i, i+1)
+    }
     moveItemReverse(35, 8)
   }
 }
